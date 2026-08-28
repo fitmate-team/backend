9b12b0f91e525c94f52b53b42167be3c8e179763
nayeong29 <kny@swu.ac.kr>
2026-08-28 21:20:57 +0900

    feat: #1 DTO 스웨거 코드 추가

--- src/main/java/com/fitmate/backend/member/dto/SignUpRequestDto.java
+++ src/main/java/com/fitmate/backend/member/dto/SignUpRequestDto.java
@@ -4,18 +4,22 @@ import com.fitmate.backend.member.domain.ExerciseGoal;
 import com.fitmate.backend.member.domain.ExerciseLevel;
 import com.fitmate.backend.member.domain.Gender;
 import com.fitmate.backend.member.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "회원가입 요청 DTO")
 @NoArgsConstructor // 기본 생성자
 @Getter
 public class SignUpRequestDto {
 
+    @Schema(description = "로그인 아이디 (8~15자)", example = "asdf1234")
     @NotBlank(message = "아이디를 입력해주세요")
     @Size(min = 8, max = 15)
     private String loginId;
 
+    @Schema(description = "비밀번호 (영문, 숫자 포함 10자 이상)", example = "asdfasdf1234")
     @NotBlank(message = "비밀번호를 입력해주세요")
     @Pattern(
         regexp = "^(?=.*[A-Za-z])(?=.*\\d).{10,}$",
@@ -23,27 +27,34 @@ public class SignUpRequestDto {
     )
     private String password;
 
+    @Schema(description = "사용자 닉네임 (2~10자)", example = "체리")
     @NotBlank(message = "닉네임을 입력해주세요")
     @Size(min = 2, max = 10)
     private String nickname;
 
+    @Schema(description = "성별 (예: MALE, FEMALE)", example = "FEMALE")
     @NotNull
     private Gender gender;
 
+    @Schema(description = "키", example = "165.5")
     @NotNull
     @Positive
     private Double height;
 
+    @Schema(description = "몸무게", example = "55.4")
     @NotNull
     @Positive
     private Double weight;
 
+    @Schema(description = "운동 수준 (예: HIGH, MEDIUM, LOW", example = "LOW")
     @NotNull
     private ExerciseLevel exerciseLevel;
 
+    @Schema(description = "운동 목표 (예: DIET, MUSCLE_GAIN, FITNESS)", example = "DIET")
     @NotNull
     private ExerciseGoal exerciseGoal;
 
+    @Schema(description = "목표 몸무게 (선택값)", example = "50.0")
     @Positive
     private Double targetWeight;
 
