a6d866511cd285ebdf0406aefc8c8390ebd12109
nayeong29 <kny@swu.ac.kr>
2026-08-28 22:38:15 +0900

    feat: #1 swagger 경로 열어주기

--- src/main/java/com/fitmate/backend/global/config/SecurityConfig.java
+++ src/main/java/com/fitmate/backend/global/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.fitmate.backend.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +14,24 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**",
+                                         "/swagger-ui.html",
+                                         "/api-docs/**",
+                                         "/v3/api-docs/**",
+                                         "/api/members/signup")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                );
+
+        return http.build();
+    }
+
+
 }
