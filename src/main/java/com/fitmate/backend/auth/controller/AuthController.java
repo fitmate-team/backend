3997a33ec7d4e7b7ce9bb7b3964eb043eb81f2e8
nayeong29 <kny@swu.ac.kr>
2026-08-29 00:28:53 +0900

    feat: #1 로그인 컨트롤러 구현

--- src/main/java/com/fitmate/backend/auth/controller/AuthController.java
+++ src/main/java/com/fitmate/backend/auth/controller/AuthController.java
@@ -1,4 +1,29 @@
 package com.fitmate.backend.auth.controller;
 
+import com.fitmate.backend.auth.dto.LoginRequestDto;
+import com.fitmate.backend.auth.dto.LoginResponseDto;
+import com.fitmate.backend.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+@Tag(name = "로그인 API", description = "로그인")
 public class AuthController {
+    private final AuthService authService;
+
+    @Operation(summary = "로그인")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
+        LoginResponseDto responseDto = authService.login(loginRequestDto);
+        return ResponseEntity.ok(responseDto);
+    }
 }
