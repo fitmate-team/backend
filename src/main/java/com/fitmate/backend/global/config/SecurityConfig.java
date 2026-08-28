70c7779b5f5ce3ec4e602a9da8ca1d2b1d9acea1
nayeong29 <kny@swu.ac.kr>
2026-08-29 00:27:52 +0900

    feat: #1 Spring Security 설정

--- src/main/java/com/fitmate/backend/global/config/SecurityConfig.java
+++ src/main/java/com/fitmate/backend/global/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.fitmate.backend.global.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,21 +18,23 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/swagger-ui/**",
-                                         "/swagger-ui.html",
-                                         "/api-docs/**",
-                                         "/v3/api-docs/**",
-                                         "/api/members/signup")
+        http.csrf(csrf -> csrf.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/api-docs/**",
+                                "/v3/api-docs/**",
+                                "/api/members/signup",
+                                "/api/auth/login")
                         .permitAll()
                         .anyRequest()
-                        .authenticated()
-                );
+                        .authenticated());
 
         return http.build();
     }
-
 
 }
