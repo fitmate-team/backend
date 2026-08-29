9949f454e488ebf6a6ffca8be2d0d7183ac40475
nayeong29 <kny@swu.ac.kr>
2026-08-30 00:22:41 +0900

    feat: #1 JWT SecurityConfig 인증 실패 EntryPoint 지정 + JWT 필터 체인 등록

--- src/main/java/com/fitmate/backend/global/config/SecurityConfig.java
+++ src/main/java/com/fitmate/backend/global/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.fitmate.backend.global.config;
 
+import com.fitmate.backend.global.security.CustomAuthenticationEntryPoint;
+import com.fitmate.backend.global.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +10,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -19,6 +27,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(
+                        customAuthenticationEntryPoint))
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
@@ -33,7 +43,9 @@ public class SecurityConfig {
                                 "/api/members/id-check")
                         .permitAll()
                         .anyRequest()
-                        .authenticated());
+                        .authenticated())
+                .addFilterBefore(jwtAuthenticationFilter,
+                                 UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
