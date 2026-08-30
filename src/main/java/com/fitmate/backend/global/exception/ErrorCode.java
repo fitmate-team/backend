9be959a81a1fa0b3f9624bf1a93c7bc215c76350
nayeong29 <kny@swu.ac.kr>
2026-08-31 03:49:18 +0900

    feat: #1 토큰 관련 에러코드 추가

--- src/main/java/com/fitmate/backend/global/exception/ErrorCode.java
+++ src/main/java/com/fitmate/backend/global/exception/ErrorCode.java
@@ -12,8 +12,11 @@ public enum ErrorCode {
     // 회원가입
     DUPLICATE_LOGIN_ID("이미 존재하는 ID 입니다.", HttpStatus.CONFLICT),
     MEMBER_NOT_FOUND("해당 회원이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-    INVALID_PASSWORD("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST);
+    INVALID_PASSWORD("비밀번호가 일치하지 않습니다.", HttpStatus.BAD_REQUEST),
 
+    // 토큰
+    REFRESH_TOKEN_EXPIRED("리프레시 토큰이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_REFRESH_TOKEN("유효하지 않은 리프레시 토큰입니다.", HttpStatus.UNAUTHORIZED);
 
     private final String message;
     private final HttpStatus httpStatus;
