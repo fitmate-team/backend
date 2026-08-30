aee9782d0f7db047b8704ca5fa60fbce79ac8a8d
nayeong29 <kny@swu.ac.kr>
2026-08-31 02:56:07 +0900

    refactor: #1 refresh 토큰 엔티티 + 레포 구현

--- src/main/java/com/fitmate/backend/auth/token/RefreshToken.java
+++ src/main/java/com/fitmate/backend/auth/token/RefreshToken.java
@@ -1,4 +1,36 @@
 package com.fitmate.backend.auth.token;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private Long memberId;
+
+    @Column(nullable = false, length = 500)
+    private String refreshToken;
+
+    @Builder
+    public RefreshToken(Long memberId, String refreshToken) {
+        this.memberId = memberId;
+        this.refreshToken = refreshToken;
+    }
+
+    // Update
+    public void updateToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
 }
--- src/main/java/com/fitmate/backend/auth/token/RefreshTokenRepository.java
+++ src/main/java/com/fitmate/backend/auth/token/RefreshTokenRepository.java
@@ -1,4 +1,9 @@
 package com.fitmate.backend.auth.token;
 
-public class RefreshTokenRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByMemberId(Long memberId);
 }
