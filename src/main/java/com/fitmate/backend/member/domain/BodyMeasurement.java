497edddac2485019d9fc6e252e102d3675e0f1ad
nayeong29 <kny@swu.ac.kr>
2026-09-18 00:12:21 +0900

    feat: #1 BodyMeasurement.java 구현

--- src/main/java/com/fitmate/backend/member/domain/BodyMeasurement.java
+++ src/main/java/com/fitmate/backend/member/domain/BodyMeasurement.java
@@ -1,13 +1,40 @@
 package com.fitmate.backend.member.domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fitmate.backend.global.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public class BodyMeasurement {
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BodyMeasurement extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    pr
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private Double weight;
+    private Double skeletalMuscleMass;
+    private Double bodyFatPercentage;
+    private Double bodyFatMass;
+
+    @Builder
+    public BodyMeasurement(Member member,
+                           Double weight,
+                           Double skeletalMuscleMass,
+                           Double bodyFatPercentage,
+                           Double bodyFatMass) {
+        this.member = member;
+        this.weight = weight;
+        this.skeletalMuscleMass = skeletalMuscleMass;
+        this.bodyFatPercentage = bodyFatPercentage;
+        this.bodyFatMass = bodyFatMass;
+    }
 }
