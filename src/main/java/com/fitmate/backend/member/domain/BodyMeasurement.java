55e05a8aa0585e4b22fb4c59aec9e387a3648472
nayeong29 <kny@swu.ac.kr>
2026-09-17 23:49:57 +0900

    feat: #1 MemberProfile.java 구현

--- src/main/java/com/fitmate/backend/member/domain/BodyMeasurement.java
+++ src/main/java/com/fitmate/backend/member/domain/BodyMeasurement.java
@@ -1,4 +1,13 @@
 package com.fitmate.backend.member.domain;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class BodyMeasurement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    pr
 }
