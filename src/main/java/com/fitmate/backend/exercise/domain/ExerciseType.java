77d408b5d645bdf39d65e36e6d7bfb1e3c7f1002
nayeong29 <kny@swu.ac.kr>
2026-09-20 01:31:50 +0900

    feat: #1 exercise 마스터 DB Enum 값 작성

--- src/main/java/com/fitmate/backend/exercise/domain/ExerciseType.java
+++ src/main/java/com/fitmate/backend/exercise/domain/ExerciseType.java
@@ -1,4 +1,8 @@
 package com.fitmate.backend.exercise.domain;
 
 public enum ExerciseType {
+    STRENGTH,
+    CORE,
+    HIIT,
+    CARDIO
 }
--- src/main/java/com/fitmate/backend/exercise/domain/MovementPattern.java
+++ src/main/java/com/fitmate/backend/exercise/domain/MovementPattern.java
@@ -1,4 +1,20 @@
 package com.fitmate.backend.exercise.domain;
 
 public enum MovementPattern {
+    HORIZONTAL_PUSH,
+    ELBOW_EXTENSION,
+    VERTICAL_PULL,
+    HINGE,
+    SHOULDER_ISOLATION,
+    HORIZONTAL_PULL,
+    VERTICAL_PUSH,
+    SQUAT,
+    LUNGE,
+    HIP_EXTENSION,
+    KNEE_ISOLATION,
+    HIP_ISOLATION,
+    ELBOW_FLEXION,
+    CORE,
+    FULL_BODY,
+    CARDIO
 }
