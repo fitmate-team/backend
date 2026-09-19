b7b3eac87421ebbd389d2927e2308d9110754d67
nayeong29 <kny@swu.ac.kr>
2026-09-20 01:35:44 +0900

    feat: #1 Equipment 마스터 DB Entity + Enum 구현

--- src/main/java/com/fitmate/backend/equipment/domain/Equipment.java
+++ src/main/java/com/fitmate/backend/equipment/domain/Equipment.java
@@ -1,4 +1,31 @@
 package com.fitmate.backend.equipment.domain;
 
+import jakarta.persistence.*;
+import jdk.jfr.Category;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Equipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String equipmentCode;
+
+    @Column(nullable = false)
+    private String nameKo;
+
+    @Column(nullable = false)
+    private String nameEn;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private EquipmentCategory category;
+
 }
